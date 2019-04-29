fa1ad82263ac2d339da66d417d0939853a97ccb2
ggq89 <ggq89@qq.com>
2019-04-29 22:17:24 +0800

    Update partial_sorts_test.go

--- partial_sorts_test.go
+++ partial_sorts_test.go
@@ -11,78 +11,60 @@ var float64s = [...]float64{74.3, 59.0, math.Inf(1), 238.2, -784.0, 2.3, math.Na
 	math.NaN(), math.Inf(-1), 9845.768, -959.7485, 905, 7.8, 7.8}
 var strings = [...]string{"", "Hello", "foo", "bar", "foo", "f00", "%*&^*&^&", "***"}
 
-func testPartialSortIntSlice(t *testing.T, fn PartialSortFn) {
-	data := ints
-
-	a := sort.IntSlice(data[0:])
-	n := a.Len()
-
-	m := 4
-	p := sort.IntSlice(data[:m])
-
-	fn(a, m)
-	if !sort.IsSorted(p) {
-		t.Errorf("sorted %v", ints)
-		t.Errorf("   got %v", data)
-		return
+func IsPartialSorted(data sort.Interface, m int) bool {
+	mid := m-1
+	for i := mid; i > 0; i-- {
+		if data.Less(i, i-1) {
+			return false
+		}
 	}
 
+	n := data.Len()
 	for i := m; i < n; i++ {
-		if a.Less(i, m-1) {
-			t.Errorf("sorted %v", ints)
-			t.Errorf("   got %v", data)
+		if data.Less(i, mid) {
+			return false
 		}
-		return
+	}
+
+	return true
+}
+
+func testPartialSortIntSlice(t *testing.T, fn PartialSortFn) {
+	data := ints
+	a := sort.IntSlice(data[0:])
+
+	m := 4
+	fn(a, m)
+
+	if !IsPartialSorted(a, m) {
+		t.Errorf("sorted %v", ints)
+		t.Errorf("   got %v", data)
 	}
 }
 
 func testPartialSortFloat64Slice(t *testing.T, fn PartialSortFn) {
 	data := float64s
-
 	a := sort.Float64Slice(data[0:])
-	n := a.Len()
 
 	m := 5
-	p := sort.Float64Slice(data[:m])
-
 	fn(a, m)
-	if !sort.IsSorted(p) {
-		t.Errorf("sorted %v", float64s)
-		t.Errorf("   got %v", data)
-		return
-	}
 
-	for i := m; i < n; i++ {
-		if a.Less(i, m-1) {
-			t.Errorf("sorted %v", ints)
-			t.Errorf("   got %v", data)
-			return
-		}
+	if !IsPartialSorted(a, m) {
+		t.Errorf("sorted %v", ints)
+		t.Errorf("   got %v", data)
 	}
 }
 
 func testPartialSortStringSlice(t *testing.T, fn PartialSortFn) {
 	data := strings
-
 	a := sort.StringSlice(data[0:])
-	n := a.Len()
 
 	m := 3
-	p := sort.StringSlice(data[:m])
-
 	fn(a, m)
-	if !sort.IsSorted(p) {
-		t.Errorf("sorted %v", strings)
-		t.Errorf("   got %v", data)
-		return
-	}
 
-	for i := m; i < n; i++ {
-		if a.Less(i, m-1) {
-			t.Errorf("sorted %v", ints)
-			t.Errorf("   got %v", data)
-			return
-		}
+	if !IsPartialSorted(a, m) {
+		t.Errorf("sorted %v", ints)
+		t.Errorf("   got %v", data)
 	}
 }
 
